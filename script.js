62435011ed5138caa43e77d34b9af9a3ffbf9763
EllieAdam <arttaielli@gmail.com>
2015-05-30 23:13:10 -0700

    bonfire #34

--- script.js
+++ script.js
@@ -643,3 +643,31 @@ function isEqual(a, b) {
     }
     return true;
 }
+
+34.
+Question: What will the code below output to the console.log and why?
+var myObject = {
+    foo: "bar",
+    func: function() {
+        var self = this;
+        console.log("outer func:  this.foo = " + this.foo);
+        console.log("outer func:  self.foo = " + self.foo);
+        (function() {
+            console.log("inner func:  this.foo = " + this.foo);
+            console.log("inner func:  self.foo = " + self.foo);
+        }());
+    }
+};
+myObject.func();
+
+Answer:
+outer func:  this.foo = bar
+outer func:  self.foo = bar
+inner func:  this.foo = undefined
+inner func:  self.foo = bar
+
+
+1. In the outer function, both this and self refer to myObject and therefore both can properly reference and access foo.
+
+2. In the inner function, though, this no longer refers to myObject. As a result, this.foo is undefined in the inner function.
+3. whereas the reference to the local variable self remains in scope and is accessible there.
