04d302712eab4fdf3bcb3148161c7a3f61e630e4
EllieAdam <arttaielli@gmail.com>
2015-07-11 23:30:52 -0700

    72

--- script.js
+++ script.js
@@ -1450,7 +1450,14 @@ if (this.hours >= 24)
 };
 var clock = new my_Clock();
   clock.run();
-55.Question: ?
+
+
+55.Question:
+Write a program to calculate the area and perimeter of a circle.
+
+Note : Create two methods to calculate the area and perimeter. The radius of the circle will be supplied by the user.
+
+
 Difficulty: 1....
 
 options:
@@ -1458,6 +1465,25 @@ options:
 2.expect(find('')).to.be.a('');
 3.expect(find('')).to.be.a('');
 4.expect(find('')).to.be.a('');
+
+Ans.
+function circle(radius)
+{
+    this.radius = radius;
+  // area method
+    this.area = function ()
+    {
+        return Math.PI * this.radius * this.radius;
+    };
+  // perimeter method
+    this.perimeter = function ()
+    {
+        return 2*Math.PI*this.radius;
+    };
+}
+var c = new circle(3);
+console.log('Area =', c.area().toFixed(2));
+console.log('perimeter =', c.perimeter().toFixed(2));
 
 56.Question: ?
 Difficulty: 1....
