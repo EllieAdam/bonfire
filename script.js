7a6d2404833e9719478a9f094f11f9799134aab0
EllieAdam <arttaielli@gmail.com>
2015-11-05 22:20:34 -0800

    194

--- script.js
+++ script.js
@@ -3724,6 +3724,20 @@ options:
 Answer
 
 
+
+Queston: 161: 
+Find the missing number??
+Difficulty: 3....
+
+options:
+1.expect(find('')).to.be.a('');
+2.expect(find('')).to.be.a('');
+3.expect(find('')).to.be.a('');
+4.expect(find('')).to.be.a('');
+Answer
+
+
+
 Queston: 162: 
 Find the closure??
 Difficulty: 3....
