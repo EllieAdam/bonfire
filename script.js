dfc9a909345a9ff4a901a736a242914aaba07d6e
EllieAdam <arttaielli@gmail.com>
2015-11-09 21:32:05 -0800

    201

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+Q-1 creat a circle. it should disappear on click disappear??
+Difficulty: 1....
+
+options:
+1.expect(find('')).to.be.a('');
+2.expect(find('')).to.be.a('');
+3.expect(find('')).to.be.a('');
+4.expect(find('')).to.be.a('');
+Answer
+
+document.getElementById("circle").onclick=function({
+  document.getElementById("circle").style.display="none";
+})
+
+
+
+
+
+
+
 Q-1.
 Callback??
 Difficulty: 1....
