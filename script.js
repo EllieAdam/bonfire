f9dbd1c21d822ead12094d41d28e15fea9ce9b38
EllieAdam <arttaielli@gmail.com>
2016-01-01 12:55:03 -0800

    275

--- script.js
+++ script.js
@@ -38,6 +38,27 @@ arrayString=arrayString+myArray[i]+" ";
     
 document.getElementById("arrayString").innerHTML=arrayString;
 
+Q. Promises in angularJS
+ 
+ // function somewhere in father-controller.js
+        var makePromiseWithSon = function() {
+            // This service's function returns a promise, but we'll deal with that shortly
+            SonService.getWeather()
+                // then() called when son gets back
+                .then(function(data) {
+                    // promise fulfilled
+                    if (data.forecast==='good') {
+                        prepareFishingTrip();
+                    } else {
+                        prepareSundayRoastDinner();
+                    }
+                }, function(error) {
+                    // promise rejected, could log the error with: console.log('error', error);
+                    prepareSundayRoastDinner();
+                });
+        };
+
+
 
 
 Q-1.
