afbbc67b03b1242660739f496eda60ff4235fa43
EllieAdam <arttaielli@gmail.com>
2016-01-18 23:34:53 -0800

    296

--- script.js
+++ script.js
@@ -11,6 +11,17 @@ document.getElementById("circle").onclick=function({
   document.getElementById("circle").style.display="none";
 })
 
+Q-1 ??
+Difficulty: 1....
+
+options:
+1.expect(find('')).to.be.a('');
+2.expect(find('')).to.be.a('');
+3.expect(find('')).to.be.a('');
+4.expect(find('')).to.be.a('');
+Answer.
+
+
 Q-1 creat a shout alert in UpperCase??
 Difficulty: 1....
 
