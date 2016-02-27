cbe6fa8b60e7edd1f4d9a974d2226704a7a0ab5f
Elliescode <arttaielli@gmail.com>
2016-02-27 15:53:24 -0800

    Update bonfire.js

--- bonfire.js
+++ bonfire.js
@@ -515,3 +515,18 @@ Question:
 
 
 Answer:
+function convert(celsius) {
+  // Only change code below this line
+  
+  var fahrenheit = celsius * 9/5 + 32;
+  // Only change code above this line
+  if ( typeof fahrenheit !== 'undefined' ) {
+    return fahrenheit;
+  } else {
+    return 'fahrenheit not defined';
+  }
+}
+
+// Change the inputs below to test your code
+convert(30);
+
