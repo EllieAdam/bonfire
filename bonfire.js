586711883d02a8b5bfc561044ce9ffc2dd3f2fb2
Elliescode <arttaielli@gmail.com>
2016-02-27 16:19:50 -0800

    Update bonfire.js

--- bonfire.js
+++ bonfire.js
@@ -524,6 +524,18 @@ function convert(celsius) {
     return 'fahrenheit not defined';
   }
 }
+Question:
+Push ["dog", 3] onto the end of the myArray variable.
+Answer:
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]); 
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+
+myArray.push(["dog", 3]);
 
 convert(30);
 
