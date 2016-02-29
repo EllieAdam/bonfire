1a98653bf40b679d661056fc2ac0574b864c175f
Elliescode <arttaielli@gmail.com>
2016-02-28 23:49:37 -0800

    Update bonfire.js

--- bonfire.js
+++ bonfire.js
@@ -552,3 +552,43 @@ function timesFive(num) {
   return num * 5;
 }
 
+Q:
+Answer:
+var count = 0;
+
+function cc(card) {
+
+  
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count = count +1;
+      break;
+      
+    case 7:
+    case 8:
+    case 9:
+      count = count + 0;
+      break; 
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count = count - 1;
+      break; 
+    
+  }
+    
+   if (count > 0) {
+    return count + ' Bet';
+  } else {
+    return count + ' Hold';
+  }
+
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
